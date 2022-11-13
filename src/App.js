feb35bb8e18fa6ec01358540a541498fda5adf30
robbiemurphy <rmurphy987@gmail.com>
2022-11-12 20:41:54 -0600

    About and Nav

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
-import './App.css';
+import React from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <main>
+        <About />
+      </main>
     </div>
   );
 }
